f11a314b796112ff162d5036f08d3167fe46847e
Zekun Li <91501532+zekunli0115@users.noreply.github.com>
2023-11-08 11:03:02 -0500

    Add files via upload

---
+++ sign.py
@@ -0,0 +1,24 @@
+import eth_account
+from web3 import Web3
+from eth_account.messages import encode_defunct
+from eth_account import Account
+
+
+def sign(m):
+    w3 = Web3()
+    # create an eth account and recover the address (derived from the public key) and private key
+    # your code here
+    if w3.is_connected():
+        account = Account.create()
+
+        eth_address = account.address  # Eth account
+        private_key = account.key.hex()
+
+    # generate signature
+    # your code here
+    message = encode_defunct(text=m)
+    signed_message = w3.eth.account.sign_message(message, private_key=private_key)
+
+    assert isinstance(signed_message, eth_account.datastructures.SignedMessage)
+
+    return eth_address, signed_message
